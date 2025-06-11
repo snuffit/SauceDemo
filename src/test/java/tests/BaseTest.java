8d8b030d5604bb08055b303c969bbe13483b8193
bludich <sadpufikot@gmail.com>
2025-06-11 20:03:53 +0300

    add headless

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -40,6 +40,7 @@ public class BaseTest {
             options.addArguments("--disable-notifications");
             options.addArguments("--disable-popup-blocking");
             options.addArguments("--disable-infobars");
+            options.addArguments("--headless");
             driver = new ChromeDriver(options);
         } else if(browser.equals("firefox")){
             driver = new FirefoxDriver();
@@ -60,6 +61,8 @@ public class BaseTest {
         if (ITestResult.FAILURE == result.getStatus()){
             takeScreenshot(driver);
         }
-        driver.quit();
+        if(driver != null) {
+            driver.quit();
+        }
     }
 }
